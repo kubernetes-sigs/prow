ibzib/test-infra

Prow: add links to Refs and pass Refs to client

prow/gerrit/adapter/adapter.go
@@ -26,6 +26,7 @@ import (
 	"os"
 	"path/filepath"
 	"strconv"
+	"strings"
 	"time"
 
 	"github.com/sirupsen/logrus"
@@ -193,13 +194,42 @@ func listChangedFiles(changeInfo client.ChangeInfo) []string {
 	return changed
 }
 
-// ProcessChange creates new presubmit prowjobs base off the gerrit changes
-func (c *Controller) ProcessChange(instance string, change client.ChangeInfo) error {
+func createRefs(reviewHost string, change client.ChangeInfo, cloneURI *url.URL, baseSHA string) (kube.Refs, error) {
 	rev, ok := change.Revisions[change.CurrentRevision]
 	if !ok {
-		return fmt.Errorf("cannot find current revision for change %v", change.ID)
+		return kube.Refs{}, fmt.Errorf("cannot find current revision for change %v", change.ID)
 	}
+	var codeHost string // Something like https://android.googlesource.com
+	parts := strings.SplitN(reviewHost, ".", 2)
+	codeHost = strings.TrimSuffix(parts[0], "-review")
+	if len(parts) > 1 {
+		codeHost += "." + parts[1]
+	}
+	kr := kube.Refs{
+		Org:      cloneURI.Host,  // Something like android-review.googlesource.com
+		Repo:     change.Project, // Something like platform/build
+		BaseRef:  change.Branch,
+		BaseSHA:  baseSHA,
+		CloneURI: cloneURI.String(), // Something like https://android-review.googlesource.com/platform/build
+		RepoLink: fmt.Sprintf("%s/%s", codeHost, change.Project),
+		BaseLink: fmt.Sprintf("%s/%s/+/%s", codeHost, change.Project, baseSHA),
+		Pulls: []kube.Pull{
+			{
+				Number:     change.Number,
+				Author:     rev.Commit.Author.Name,
+				SHA:        change.CurrentRevision,
+				Ref:        rev.Ref,
+				Link:       fmt.Sprintf("%s/c/%s/+/%d", reviewHost, change.Project, change.Number),
+				CommitLink: fmt.Sprintf("%s/%s/+/%s", codeHost, change.Project, change.CurrentRevision),
+				AuthorLink: fmt.Sprintf("%s/q/%s", reviewHost, rev.Commit.Author.Email),
+			},
+		},
+	}
+	return kr, nil
+}
 
+// ProcessChange creates new presubmit prowjobs base off the gerrit changes
+func (c *Controller) ProcessChange(instance string, change client.ChangeInfo) error {
 	logger := logrus.WithField("gerrit change", change.Number)
 
 	cloneURI, err := makeCloneURI(instance, change.Project)
@@ -214,20 +244,9 @@ func (c *Controller) ProcessChange(instance string, change client.ChangeInfo) er
 
 	triggeredJobs := []string{}
 
-	kr := kube.Refs{
-		Org:      cloneURI.Host,  // Something like android.googlesource.com
-		Repo:     change.Project, // Something like platform/build
-		BaseRef:  change.Branch,
-		BaseSHA:  baseSHA,
-		CloneURI: cloneURI.String(), // Something like https://android.googlesource.com/platform/build
-		Pulls: []kube.Pull{
-			{
-				Number: change.Number,
-				Author: rev.Commit.Author.Name,
-				SHA:    change.CurrentRevision,
-				Ref:    rev.Ref,
-			},
-		},
+	kr, err := createRefs(instance, change, cloneURI, baseSHA)
+	if err != nil {
+		return fmt.Errorf("failed to get refs: %v", err)
 	}
 
 	type jobSpec struct {

prow/pjutil/pjutil_test.go
@@ -28,6 +28,7 @@ import (
 	"k8s.io/apimachinery/pkg/util/diff"
 
 	"k8s.io/test-infra/prow/config"
+	"k8s.io/test-infra/prow/github"
 	"k8s.io/test-infra/prow/kube"
 )
 
@@ -648,5 +649,50 @@ func TestJobURL(t *testing.T) {
 				t.Errorf("%s: expected URL to be %q but got %q", testCase.name, expected, actual)
 			}
 		})
+	}
+}
+
+func TestCreateRefs(t *testing.T) {
+	pr := github.PullRequest{
+		Number:  42,
+		HTMLURL: "https://github.example.com/kubernetes/Hello-World/pull/42",
+		Head: github.PullRequestBranch{
+			SHA: "123456",
+		},
+		Base: github.PullRequestBranch{
+			Ref: "master",
+			Repo: github.Repo{
+				Name:    "Hello-World",
+				HTMLURL: "https://github.example.com/kubernetes/Hello-World",
+				Owner: github.User{
+					Login: "kubernetes",
+				},
+			},
+		},
+		User: github.User{
+			Login:   "ibzib",
+			HTMLURL: "https://github.example.com/ibzib",
+		},
+	}
+	expected := kube.Refs{
+		Org:      "kubernetes",
+		Repo:     "Hello-World",
+		RepoLink: "https://github.example.com/kubernetes/Hello-World",
+		BaseRef:  "master",
+		BaseSHA:  "abcdef",
+		BaseLink: "https://github.example.com/kubernetes/Hello-World/commit/abcdef",
+		Pulls: []kube.Pull{
+			{
+				Number:     42,
+				Author:     "ibzib",
+				SHA:        "123456",
+				Link:       "https://github.example.com/kubernetes/Hello-World/pull/42",
+				AuthorLink: "https://github.example.com/ibzib",
+				CommitLink: "https://github.example.com/kubernetes/Hello-World/pull/42/commits/123456",
+			},
+		},
+	}
+	if actual := createRefs(pr, "abcdef"); !reflect.DeepEqual(expected, actual) {
+		t.Errorf("diff between expected and actual refs:%s", diff.ObjectReflectDiff(expected, actual))
 	}
 }

prow/plugins/trigger/push.go
@@ -17,6 +17,8 @@ limitations under the License.
 package trigger
 
 import (
+	"fmt"
+
 	"k8s.io/test-infra/prow/github"
 	"k8s.io/test-infra/prow/kube"
 	"k8s.io/test-infra/prow/pjutil"
@@ -42,6 +44,16 @@ func listPushEventChanges(pe github.PushEvent) []string {
 	return changedFiles
 }
 
+func createRefs(pe github.PushEvent) kube.Refs {
+	return kube.Refs{
+		Org:      pe.Repo.Owner.Name,
+		Repo:     pe.Repo.Name,
+		BaseRef:  pe.Branch(),
+		BaseSHA:  pe.After,
+		BaseLink: fmt.Sprintf("%s/commit/%s", pe.Repo.HTMLURL, pe.After),
+	}
+}
+
 func handlePE(c Client, pe github.PushEvent) error {
 	if pe.Deleted {
 		// we should not trigger jobs for a branch deletion
@@ -55,12 +67,7 @@ func handlePE(c Client, pe github.PushEvent) error {
 		if !j.RunsAgainstChanges(changedFiles) {
 			continue
 		}
-		kr := kube.Refs{
-			Org:     pe.Repo.Owner.Name,
-			Repo:    pe.Repo.Name,
-			BaseRef: pe.Branch(),
-			BaseSHA: pe.After,
-		}
+		kr := createRefs(pe)
 		labels := make(map[string]string)
 		for k, v := range j.Labels {
 			labels[k] = v

kubernetes-sigs/prow

prow/gerrit/adapter/adapter.go
@@ -26,6 +26,7 @@ import (
 	"os"
 	"path/filepath"
 	"strconv"
+	"strings"
 	"time"
 
 	"github.com/sirupsen/logrus"
@@ -196,13 +197,42 @@ func listChangedFiles(changeInfo client.ChangeInfo) config.ChangedFilesProvider 
 	}
 }
 
-// ProcessChange creates new presubmit prowjobs base off the gerrit changes
-func (c *Controller) ProcessChange(instance string, change client.ChangeInfo) error {
+func createRefs(reviewHost string, change client.ChangeInfo, cloneURI *url.URL, baseSHA string) (prowapi.Refs, error) {
 	rev, ok := change.Revisions[change.CurrentRevision]
 	if !ok {
-		return fmt.Errorf("cannot find current revision for change %v", change.ID)
+		return prowapi.Refs{}, fmt.Errorf("cannot find current revision for change %v", change.ID)
 	}
+	var codeHost string // Something like https://android.googlesource.com
+	parts := strings.SplitN(reviewHost, ".", 2)
+	codeHost = strings.TrimSuffix(parts[0], "-review")
+	if len(parts) > 1 {
+		codeHost += "." + parts[1]
+	}
+	refs := prowapi.Refs{
+		Org:      cloneURI.Host,  // Something like android-review.googlesource.com
+		Repo:     change.Project, // Something like platform/build
+		BaseRef:  change.Branch,
+		BaseSHA:  baseSHA,
+		CloneURI: cloneURI.String(), // Something like https://android-review.googlesource.com/platform/build
+		RepoLink: fmt.Sprintf("%s/%s", codeHost, change.Project),
+		BaseLink: fmt.Sprintf("%s/%s/+/%s", codeHost, change.Project, baseSHA),
+		Pulls: []prowapi.Pull{
+			{
+				Number:     change.Number,
+				Author:     rev.Commit.Author.Name,
+				SHA:        change.CurrentRevision,
+				Ref:        rev.Ref,
+				Link:       fmt.Sprintf("%s/c/%s/+/%d", reviewHost, change.Project, change.Number),
+				CommitLink: fmt.Sprintf("%s/%s/+/%s", codeHost, change.Project, change.CurrentRevision),
+				AuthorLink: fmt.Sprintf("%s/q/%s", reviewHost, rev.Commit.Author.Email),
+			},
+		},
+	}
+	return refs, nil
+}
 
+// ProcessChange creates new presubmit prowjobs base off the gerrit changes
+func (c *Controller) ProcessChange(instance string, change client.ChangeInfo) error {
 	logger := logrus.WithField("gerrit change", change.Number)
 
 	cloneURI, err := makeCloneURI(instance, change.Project)
@@ -217,20 +247,9 @@ func (c *Controller) ProcessChange(instance string, change client.ChangeInfo) er
 
 	triggeredJobs := []string{}
 
-	kr := prowapi.Refs{
-		Org:      cloneURI.Host,  // Something like android.googlesource.com
-		Repo:     change.Project, // Something like platform/build
-		BaseRef:  change.Branch,
-		BaseSHA:  baseSHA,
-		CloneURI: cloneURI.String(), // Something like https://android.googlesource.com/platform/build
-		Pulls: []prowapi.Pull{
-			{
-				Number: change.Number,
-				Author: rev.Commit.Author.Name,
-				SHA:    change.CurrentRevision,
-				Ref:    rev.Ref,
-			},
-		},
+	refs, err := createRefs(instance, change, cloneURI, baseSHA)
+	if err != nil {
+		return fmt.Errorf("failed to get refs: %v", err)
 	}
 
 	type jobSpec struct {
@@ -251,7 +270,7 @@ func (c *Controller) ProcessChange(instance string, change client.ChangeInfo) er
 				return fmt.Errorf("failed to determine if postsubmit %q should run: %v", postsubmit.Name, err)
 			} else if shouldRun {
 				jobSpecs = append(jobSpecs, jobSpec{
-					spec:   pjutil.PostsubmitSpec(postsubmit, kr),
+					spec:   pjutil.PostsubmitSpec(postsubmit, refs),
 					labels: postsubmit.Labels,
 				})
 			}
@@ -264,7 +283,7 @@ func (c *Controller) ProcessChange(instance string, change client.ChangeInfo) er
 				return fmt.Errorf("failed to determine if presubmit %q should run: %v", presubmit.Name, err)
 			} else if shouldRun {
 				jobSpecs = append(jobSpecs, jobSpec{
-					spec:   pjutil.PresubmitSpec(presubmit, kr),
+					spec:   pjutil.PresubmitSpec(presubmit, refs),
 					labels: presubmit.Labels,
 				})
 			}

prow/pjutil/pjutil_test.go
@@ -29,6 +29,7 @@ import (
 
 	prowapi "k8s.io/test-infra/prow/apis/prowjobs/v1"
 	"k8s.io/test-infra/prow/config"
+	"k8s.io/test-infra/prow/github"
 	"k8s.io/test-infra/prow/kube"
 )
 
@@ -649,5 +650,50 @@ func TestJobURL(t *testing.T) {
 				t.Errorf("%s: expected URL to be %q but got %q", testCase.name, expected, actual)
 			}
 		})
+	}
+}
+
+func TestCreateRefs(t *testing.T) {
+	pr := github.PullRequest{
+		Number:  42,
+		HTMLURL: "https://github.example.com/kubernetes/Hello-World/pull/42",
+		Head: github.PullRequestBranch{
+			SHA: "123456",
+		},
+		Base: github.PullRequestBranch{
+			Ref: "master",
+			Repo: github.Repo{
+				Name:    "Hello-World",
+				HTMLURL: "https://github.example.com/kubernetes/Hello-World",
+				Owner: github.User{
+					Login: "kubernetes",
+				},
+			},
+		},
+		User: github.User{
+			Login:   "ibzib",
+			HTMLURL: "https://github.example.com/ibzib",
+		},
+	}
+	expected := prowapi.Refs{
+		Org:      "kubernetes",
+		Repo:     "Hello-World",
+		RepoLink: "https://github.example.com/kubernetes/Hello-World",
+		BaseRef:  "master",
+		BaseSHA:  "abcdef",
+		BaseLink: "https://github.example.com/kubernetes/Hello-World/commit/abcdef",
+		Pulls: []prowapi.Pull{
+			{
+				Number:     42,
+				Author:     "ibzib",
+				SHA:        "123456",
+				Link:       "https://github.example.com/kubernetes/Hello-World/pull/42",
+				AuthorLink: "https://github.example.com/ibzib",
+				CommitLink: "https://github.example.com/kubernetes/Hello-World/pull/42/commits/123456",
+			},
+		},
+	}
+	if actual := createRefs(pr, "abcdef"); !reflect.DeepEqual(expected, actual) {
+		t.Errorf("diff between expected and actual refs:%s", diff.ObjectReflectDiff(expected, actual))
 	}
 }

prow/plugins/trigger/push.go
@@ -45,6 +45,16 @@ func listPushEventChanges(pe github.PushEvent) config.ChangedFilesProvider {
 	}
 }
 
+func createRefs(pe github.PushEvent) prowapi.Refs {
+	return prowapi.Refs{
+		Org:      pe.Repo.Owner.Name,
+		Repo:     pe.Repo.Name,
+		BaseRef:  pe.Branch(),
+		BaseSHA:  pe.After,
+		BaseLink: pe.Compare,
+	}
+}
+
 func handlePE(c Client, pe github.PushEvent) error {
 	if pe.Deleted {
 		// we should not trigger jobs for a branch deletion
@@ -56,18 +66,13 @@ func handlePE(c Client, pe github.PushEvent) error {
 		} else if !shouldRun {
 			continue
 		}
-		kr := prowapi.Refs{
-			Org:     pe.Repo.Owner.Name,
-			Repo:    pe.Repo.Name,
-			BaseRef: pe.Branch(),
-			BaseSHA: pe.After,
-		}
+		refs := createRefs(pe)
 		labels := make(map[string]string)
 		for k, v := range j.Labels {
 			labels[k] = v
 		}
 		labels[github.EventGUID] = pe.GUID
-		pj := pjutil.NewProwJob(pjutil.PostsubmitSpec(j, kr), labels)
+		pj := pjutil.NewProwJob(pjutil.PostsubmitSpec(j, refs), labels)
 		c.Logger.WithFields(pjutil.ProwJobFields(&pj)).Info("Creating a new prowjob.")
 		if _, err := c.ProwJobClient.Create(&pj); err != nil {
 			return err