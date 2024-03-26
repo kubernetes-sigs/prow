6947186ebd7926acd0cf6bfdf64c85f31c5f5ca1
Cole Wagner <colew@google.com>
2024-03-26 15:45:42 -0700

    Fix boilerplate validation (and set up python env to do so).

--- hack/run-in-python-container.sh
+++ hack/run-in-python-container.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# Copyright 2022 The Kubernetes Authors.
+# Copyright 2021 The Kubernetes Authors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,16 +13,22 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# this script runs tslint against the repo using docker
-#
-# TODO: we should be using eslint instead now, but this repo is also
-# on a rather old version of node, first we get the build system migrated.
+# This script runs $@ in a python container
 
 set -o errexit
 set -o nounset
 set -o pipefail
 
-REPO_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/../../.." && pwd -P)"
+REPO_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd -P)"
 cd "${REPO_ROOT}"
 
-./hack/run-in-python-container.sh ./hack/boilerplate/verify_boilerplate.py
+PY_IMAGE='python:3.9-slim-buster'
+
+docker run \
+    --rm -i \
+    -e HOME=/tmp \
+    -e PYTHONPATH='' \
+    -v "${REPO_ROOT:?}:${REPO_ROOT:?}" -w "${REPO_ROOT}" \
+    --security-opt="label=disable" \
+    "${PY_IMAGE}" \
+    bash -c 'source ./hack/make-rules/py-test/activate-python_venv.sh && $0 $@' "$@"
