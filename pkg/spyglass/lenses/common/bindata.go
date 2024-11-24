9ccc6b3c86f19563c84ae11eb952621a812845bc
xiaonan chen <sawyer523@163.com>
2024-11-24 09:54:45 +0800

    fix: prow-verify-lint

--- pkg/spyglass/lenses/common/bindata.go
+++ pkg/spyglass/lenses/common/bindata.go
@@ -93,7 +93,7 @@ func staticSpyglassLensHtml() (*asset, error) {
 		return nil, err
 	}
 
-	info := bindataFileInfo{name: "static/spyglass-lens.html", size: 616, mode: os.FileMode(436), modTime: time.Unix(1724780439, 0)}
+	info := bindataFileInfo{name: "static/spyglass-lens.html", size: 616, mode: os.FileMode(420), modTime: time.Unix(1732374776, 0)}
 	a := &asset{bytes: bytes, info: info}
 	return a, nil
 }
