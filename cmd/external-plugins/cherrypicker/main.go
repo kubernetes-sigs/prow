75747a14cca39002734453d37b480ae676a89343
Aaruni Aggarwal <aaruniagg@gmail.com>
2026-04-06 00:09:01 +0530

    fixing some of the review comments
    Signed-off-by: aaragga1 <aaragga1@in.ibm.com>

--- cmd/external-plugins/cherrypicker/main.go
+++ cmd/external-plugins/cherrypicker/main.go
@@ -44,12 +44,12 @@ type options struct {
 	instrumentationOptions prowflagutil.InstrumentationOptions
 	logLevel               string
 
-	webhookSecretFile string
-	prowAssignments   bool
-	allowAll          bool
-	issueOnConflict   bool
-        addOriginalCommitID bool
-	labelPrefix       string
+	webhookSecretFile   string
+	prowAssignments     bool
+	allowAll            bool
+	issueOnConflict     bool
+	addOriginalCommitID bool
+	labelPrefix         string
 }
 
 func (o *options) Validate() error {
@@ -73,7 +73,7 @@ func gatherOptions() options {
 	fs.BoolVar(&o.prowAssignments, "use-prow-assignments", true, "Use prow commands to assign cherrypicked PRs.")
 	fs.BoolVar(&o.allowAll, "allow-all", false, "Allow anybody to use automated cherrypicks by skipping GitHub organization membership checks.")
 	fs.BoolVar(&o.issueOnConflict, "create-issue-on-conflict", false, "Create a GitHub issue and assign it to the requestor on cherrypick conflict.")
-        fs.BoolVar(&o.addOriginalCommitID, "add-original-commit-id", true, "Add original commit ID to cherry-picked commit messages (similar to git cherry-pick -x).")
+	fs.BoolVar(&o.addOriginalCommitID, "add-original-commit-id", true, "Add original commit ID to cherry-picked commit messages (similar to git cherry-pick -x).")
 	fs.StringVar(&o.labelPrefix, "label-prefix", defaultLabelPrefix, "Set a custom label prefix.")
 	for _, group := range []flagutil.OptionGroup{&o.github, &o.instrumentationOptions} {
 		group.AddFlags(fs)
@@ -133,12 +133,12 @@ func main() {
 		ghc: githubClient,
 		log: log,
 
-		labels:          o.labels.Strings(),
-		prowAssignments: o.prowAssignments,
-		allowAll:        o.allowAll,
-		issueOnConflict: o.issueOnConflict,
-                addOriginalCommitID: o.addOriginalCommitID,
-		labelPrefix:     o.labelPrefix,
+		labels:              o.labels.Strings(),
+		prowAssignments:     o.prowAssignments,
+		allowAll:            o.allowAll,
+		issueOnConflict:     o.issueOnConflict,
+		addOriginalCommitID: o.addOriginalCommitID,
+		labelPrefix:         o.labelPrefix,
 
 		bare:     &http.Client{},
 		patchURL: "https://patch-diff.githubusercontent.com",
